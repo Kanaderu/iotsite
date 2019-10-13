303f4c1216ec0f88cf166db9b7ad053daea0413a
David Fan <fand.kanade@gmail.com>
2019-10-13 06:35:00 -0400

    Adjusted Function State Hook to Class State Hook
    For more information about Function State Hooks and Class State Hooks, check out this page: https://reactjs.org/docs/hooks-state.html
    Setup for the BottomNavigation component can be found here: https://material-ui.com/api/bottom-navigation/
    The default selected value is 0 set by the constructor

--- dashboard/src/components/FooterMaterial.js
+++ dashboard/src/components/FooterMaterial.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
@@ -11,64 +11,49 @@ import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Slide from '@material-ui/core/Slide';
 import PropTypes from 'prop-types';
 
-function HideOnScrollFooter(props) {
-  const { children } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
-  const trigger = useScrollTrigger();
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  );
-}
-
-HideOnScrollFooter.propTypes = {
-  children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window: PropTypes.func,
-};
-
-const FooterMaterial = (props) => {
-    const footerStyles = makeStyles({
-      footer: {
+const styles = theme => ({
+    footer: {
         width: '100%',
         position: 'fixed',
         top: 'auto',
         bottom: 0,
-      },
-    });
+    },
+});
 
-  const classes = footerStyles();
-  const [value, setValue] = React.useState(0);
 
-  return (
-    <React.Fragment>
-    <CssBaseline />
-    <HideOnScrollFooter {...props}>
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.footer}
-    >
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-    </BottomNavigation>
-    </HideOnScrollFooter>
-    </React.Fragment>
-  );
+class FooterMaterial extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+    }
+
+        //const [value, setValue] = React.useState(0);
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <BottomNavigation
+                    value={this.state.value}
+                    onChange={(event, newValue) => {
+                        this.setState({value: newValue});
+                    }}
+                    showLabels
+                    className={classes.footer}
+                >
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                </BottomNavigation>
+            </React.Fragment>
+        )
+    }
 }
 
-export default FooterMaterial;
+export default withStyles(styles)(FooterMaterial);
