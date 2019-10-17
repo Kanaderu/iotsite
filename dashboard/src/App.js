0c91496fe107f1050084d4ac42899d6a0561f020
David Fan <fand.kanade@gmail.com>
2019-10-17 01:36:16 +0000

    Cleanup

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import Routes from './components/Routes';
-//import TopNavigation from './components/topNavigation';
 import TopNavigationMaterial from './components/topNavigationMaterial';
-//import Footer from './components/Footer';
 import FooterMaterial from './components/FooterMaterial';
 import './index.css';
 
@@ -11,12 +9,10 @@ class App extends Component {
     render() {
         return (
             <div className="flexible-content">
-                {/*<TopNavigation />*/}
                 <TopNavigationMaterial />
                 <main className="p-5">
                     <Routes />
                 </main>
-                {/*<Footer />*/}
                 <FooterMaterial />
             </div>
         );
