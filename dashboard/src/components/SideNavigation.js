f6935c701ca8f02a75638e36bcc22da45b8aebc1
David Fan <fand.kanade@gmail.com>
2019-10-20 06:16:30 +0000

    Updated sidenav to ws/

--- dashboard/src/components/SideNavigation.js
+++ dashboard/src/components/SideNavigation.js
@@ -39,7 +39,7 @@ class SideNavigation extends Component {
         return (
             <div className={classes.root}>
                 <List component="nav" aria-label="main mailbox folders">
-                    <NavLink exact={true} to="/dashboard" style={{ textDecoration: 'none' }}>
+                    <NavLink exact={true} to="/ws" style={{ textDecoration: 'none' }}>
                         <ListItem
                             button
                             selected={this.state.selectedIndex === 0}
