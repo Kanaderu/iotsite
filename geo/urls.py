d690d730db1db853f8ef7c7916c4a19f69b63821
David Fan <fand.kanade@gmail.com>
2019-10-27 18:18:23 -0400

    Renamed app from world to geo

--- geo/urls.py
+++ geo/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^worldborders.geojson$', GeoJSONLayerView.as_view(model=WorldBorder, geometry_field='mpoly', properties=('name',)), name='worldborders'),
     url(r'^linkstations.geojson$', GeoJSONLayerView.as_view(model=LINKStation, properties=('name',)), name='linkstations'),
 
-    url(r'^worldpoints$', TemplateView.as_view(template_name='world/points.html'), name='worldpoints_leaf'),
-    url(r'^worldborders$', TemplateView.as_view(template_name='world/borders.html'), name='worldborders_leaf'),
-    url(r'^linkstations$', TemplateView.as_view(template_name='world/link.html'), name='linkstations_leaf'),
+    url(r'^worldpoints$', TemplateView.as_view(template_name='geo/points.html'), name='worldpoints_leaf'),
+    url(r'^worldborders$', TemplateView.as_view(template_name='geo/borders.html'), name='worldborders_leaf'),
+    url(r'^linkstations$', TemplateView.as_view(template_name='geo/link.html'), name='linkstations_leaf'),
 ]
--- iotsite/settings/base.py
+++ iotsite/settings/base.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django.contrib.gis',
-    'world',
+    'geo',
     'djgeojson',
     'leaflet',
 
