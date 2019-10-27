ad2ce92b644eda9bd7b1b7656df6b1e503c9e516
David Fan <fand.kanade@gmail.com>
2019-10-27 03:10:49 -0400

    Added postgis scripts

---
+++ setup/geodjango/install_geospatial_libraries.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Ubuntu setup, refer to https://docs.djangoproject.com/en/2.2/ref/contrib/gis/install/geolibs/ for other distributions
+sudo apt-get install binutils libproj-dev gdal-bin
+
+# GeoIP2
+# https://docs.djangoproject.com/en/2.2/ref/contrib/gis/geoip2/
+# https://github.com/maxmind/libmaxminddb
+sudo add-apt-repository ppa:maxmind/ppa
+sudo apt update
+sudo apt install libmaxminddb0 libmaxminddb-dev mmdb-bin
