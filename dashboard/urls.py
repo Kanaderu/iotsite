59a982a92fa284f760093d5d6b1b23bc7bb6430b
David Fan <fand.kanade@gmail.com>
2019-11-02 18:01:29 -0400

    Added frontend dev support

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard/index.html')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('ws/', TemplateView.as_view(template_name='dashboard/index.html')),
+    ]
