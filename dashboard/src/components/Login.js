447b2ef3dcfa589052152c8ae25b9ade124b699b
David Fan <fand.kanade@gmail.com>
2019-11-11 15:38:17 -0500

    Redirect any other URLs to frontend

--- dashboard/src/components/Login.js
+++ dashboard/src/components/Login.js
@@ -1,56 +1,83 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import {Link, Redirect} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 
-import {auth} from "./actions";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+
+import { auth } from "./actions";
 
 class Login extends Component {
 
-  onSubmit = e => {
-    e.preventDefault();
-    this.props.login(this.state.username, this.state.password);
-  }
-
-  render() {
-    if (this.props.isAuthenticated) {
-      return <Redirect to="/" />
+    onSubmit = e => {
+        e.preventDefault();
+        this.props.login(this.state.username, this.state.password);
     }
-    return (
-      <form onSubmit={this.onSubmit}>
-        <fieldset>
-          <legend>Login</legend>
-          {/* display errors */}
-          {this.props.errors.length > 0 && (
-            <ul>
-              {this.props.errors.map(error => (
-                <li key={error.field}>{error.message}</li>
-              ))}
-            </ul>
-          )}
-          {/* build login form */}
-          <p>
-            <label htmlFor="username">Username</label>
-            <input
-              type="text" id="username"
-              onChange={e => this.setState({username: e.target.value})} />
-          </p>
-          <p>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password" id="password"
-              onChange={e => this.setState({password: e.target.value})} />
-          </p>
-          <p>
-            <button type="submit">Login</button>
-          </p>
 
-          <p>
-            Don't have an account? <Link to="/register">Register</Link>
-          </p>
-        </fieldset>
-      </form>
-    )
-  }
+    render() {
+        const { classes } = this.props;
+
+        if (this.props.isAuthenticated) {
+            return <Redirect to="/" />
+        }
+
+        return (
+            <div>
+            <Card>
+                <CardContent>
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Username"
+                        margin="normal"
+                    />
+                    <br />
+                    <TextField
+                        id="standard-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        margin="normal"
+                    />
+                </CardContent>
+            </Card>
+            <form onSubmit={this.onSubmit}>
+                <fieldset>
+                    <legend>Login</legend>
+                    {/* display errors */}
+                    {this.props.errors.length > 0 && (
+                        <ul>
+                            {this.props.errors.map(error => (
+                                <li key={error.field}>{error.message}</li>
+                            ))}
+                        </ul>
+                    )}
+                    {/* build login form */}
+                    <p>
+                        <label htmlFor="username">Username:</label>
+                        <input
+                            type="text" id="username"
+                            onChange={e => this.setState({username: e.target.value})} />
+                    </p>
+                    <p>
+                      <label htmlFor="password">Password</label>
+                      <input
+                            type="password" id="password"
+                            onChange={e => this.setState({password: e.target.value})} />
+                    </p>
+                    <p>
+                        <button type="submit">Login</button>
+                    </p>
+
+                    <p>
+                        Don't have an account? <Link to="/register">Register</Link>
+                    </p>
+                </fieldset>
+            </form>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => {
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard/index.html'), name='home'),
+    re_path(r'.*', TemplateView.as_view(template_name='dashboard/index.html'), name='misc-home')
 ]
 
 if settings.DEBUG:
     urlpatterns += [
         path('ws/', TemplateView.as_view(template_name='dashboard/index.html')),
+        re_path(r'ws/.*', TemplateView.as_view(template_name='dashboard/index.html'))
     ]
