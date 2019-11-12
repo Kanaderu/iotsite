fbcdcf952180c7e8cb76189b7109c07473960834
David Fan <fand.kanade@gmail.com>
2019-11-11 23:11:02 -0500

    Added material login and react modifications

--- dashboard/src/components/Login.js
+++ dashboard/src/components/Login.js
@@ -16,7 +16,7 @@ class Login extends Component {
     onSubmit = e => {
         e.preventDefault();
         this.props.login(this.state.username, this.state.password);
-    }
+    };
 
     render() {
         const { classes } = this.props;
@@ -29,40 +29,46 @@ class Login extends Component {
             <div>
             <Card>
                 <CardContent>
-                    {this.props.errors.length > 0 && (
-                        <ul>
-                            {this.props.errors.map(error => (
-                                <li key={error.field}>{error.message}</li>
-                            ))}
-                        </ul>
-                    )}
-                    <TextField
-                        required
-                        id="standard-required"
-                        label="Username"
-                        margin="normal"
-                    />
-                    <br />
-                    <TextField
-                        id="standard-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        margin="normal"
-                    />
-                    <br />
-                    <Button variant="contained" color="primary">
-                        Submit
-                    </Button>
-                    <Button variant="contained" color="secondary">
-                        Register
-                    </Button>
+                    <form onSubmit={this.onSubmit}>
+                        {this.props.errors.length > 0 && (
+                            <ul>
+                                {this.props.errors.map(error => (
+                                    <li key={error.field}>{error.message}</li>
+                                ))}
+                            </ul>
+                        )}
+                        <TextField
+                            required
+                            id="username"
+                            label="Username"
+                            margin="normal"
+                            onChange={e => this.setState({username: e.target.value})}
+                        />
+                        <br />
+                        <TextField
+                            id="password"
+                            label="Password"
+                            type="password"
+                            autoComplete="current-password"
+                            margin="normal"
+                            onChange={e => this.setState({password: e.target.value})}
+                        />
+                        <br />
+                        <Button type="submit" variant="contained" color="primary">
+                            Submit
+                        </Button>
+                        <Link to="/register">
+                            <Button variant="contained" color="secondary">
+                                Register
+                            </Button>
+                        </Link>
+                    </form>
                 </CardContent>
-            </Card>
+            </Card>{/*
             <form onSubmit={this.onSubmit}>
                 <fieldset>
                     <legend>Login</legend>
-                    {/* display errors */}
+
                     {this.props.errors.length > 0 && (
                         <ul>
                             {this.props.errors.map(error => (
@@ -70,7 +76,7 @@ class Login extends Component {
                             ))}
                         </ul>
                     )}
-                    {/* build login form */}
+
                     <p>
                         <label htmlFor="username">Username:</label>
                         <input
@@ -91,32 +97,31 @@ class Login extends Component {
                         Don't have an account? <Link to="/register">Register</Link>
                     </p>
                 </fieldset>
-            </form>
+            </form>*/}
             </div>
         )
     }
 }
 
 const mapStateToProps = state => {
-  let errors = [];
-  if (state.auth.errors) {
-    errors = Object.keys(state.auth.errors).map(field => {
-      return {field, message: state.auth.errors[field]};
-    });
-  }
-  return {
-    errors,
-    isAuthenticated: state.auth.isAuthenticated
-  };
-}
+    let errors = [];
+      if (state.auth.errors) {
+            errors = Object.keys(state.auth.errors).map(field => {
+                return {field, message: state.auth.errors[field]};
+            });
+      }
+      return {
+            errors,
+            isAuthenticated: state.auth.isAuthenticated
+      };
+};
 
 const mapDispatchToProps = dispatch => {
-  return {
-    login: (username, password) => {
-      return dispatch(auth.login(username, password));
-    }
-  };
-}
+    return {
+        login: (username, password) => {
+            return dispatch(auth.login(username, password));
+        }
+    };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
-
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard/index.html'), name='home'),
-    re_path(r'.*', TemplateView.as_view(template_name='dashboard/index.html'), name='misc-home')
+    path('', csrf_exempt(TemplateView.as_view(template_name='dashboard/index.html')), name='home'),
+    re_path(r'.*', csrf_exempt(TemplateView.as_view(template_name='dashboard/index.html')), name='misc-home')
 ]
 
 if settings.DEBUG:
