d476a3f725675fbe3e24882bf1b89e455dc7992b
David Fan <fand.kanade@gmail.com>
2019-11-26 08:13:25 -0500

    Added catch all URLs to DEBUG only

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,11 +5,11 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', csrf_exempt(TemplateView.as_view(template_name='dashboard/index.html')), name='home'),
-    re_path(r'.*', csrf_exempt(TemplateView.as_view(template_name='dashboard/index.html')), name='misc-home')
 ]
 
 if settings.DEBUG:
     urlpatterns += [
         path('ws/', TemplateView.as_view(template_name='dashboard/index.html')),
-        re_path(r'ws/.*', TemplateView.as_view(template_name='dashboard/index.html'))
+        re_path(r'ws/.*', TemplateView.as_view(template_name='dashboard/index.html')),
+        re_path(r'.*', csrf_exempt(TemplateView.as_view(template_name='dashboard/index.html')), name='misc-home'),
     ]
