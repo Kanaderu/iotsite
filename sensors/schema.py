526163542b5ccbb72e27ee2e6e8a7f67f1648968
David Fan <fand.kanade@gmail.com>
2020-02-08 18:36:33 -0500

    Added gis field support for graphql

--- sensors/schema.py
+++ sensors/schema.py
@@ -1,6 +1,24 @@
 import graphene
+import json
+from django.contrib.gis.db import models
 from graphene_django.types import DjangoObjectType
+from graphene_django.converter import convert_django_field
 from .models import Sensor, SensorData
+
+
+class GeoJSON(graphene.Scalar):
+
+    @classmethod
+    def serialize(cls, value):
+        return json.loads(value.geojson)
+
+
+@convert_django_field.register(models.GeometryField)
+def convert_field_to_geojson(field, registry=None):
+    return graphene.Field(
+        GeoJSON,
+        description=field.help_text,
+        required=not field.null)
 
 
 class SensorType(DjangoObjectType):
