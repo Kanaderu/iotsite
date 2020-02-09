c454a5e46819981aab52fd7b6d6dedf18ddac5aa
David Fan <fand.kanade@gmail.com>
2020-02-09 18:32:29 -0500

    Test 5

--- source/conf.py
+++ source/conf.py
@@ -46,7 +46,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
