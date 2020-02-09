331e86c94125823ecff57a4a3268072abfd24e78
David Fan <fand.kanade@gmail.com>
2020-02-08 20:11:43 -0500

    Added user api token unittests #73

--- users/tests.py
+++ users/tests.py
@@ -29,3 +29,22 @@ class UserCreationTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIsNotNone(response.json()['access'])
         self.assertIsNotNone(response.json()['refresh'])
+
+    def test_user_retrieve_api_token(self):
+        # create user account
+        self.test_create_user()
+
+        # check if created user account is able to login with refresh and access tokens provided
+        login_response = self.client.post(reverse('user-login'),
+                                    {'username': self.username, 'password': self.password},
+                                    content_type='application/json')
+        self.assertEqual(login_response.status_code, 200)
+
+        access_token = login_response.json()['access']
+        self.assertIsNotNone(access_token)
+
+        token_response = self.client.get(reverse('api-token'),
+                                   content_type='application/json',
+                                    **{'HTTP_AUTHORIZATION': 'Bearer {}'.format(access_token)})
+        self.assertEqual(token_response.status_code, 200)
+        self.assertIsNotNone(token_response.json()['token'])
