3a748769690a11e1a63d63f72b8f6a6f19d23dc1
David Fan <fand.kanade@gmail.com>
2020-02-19 03:15:56 -0500

    Modified geo db user for unittests #79

--- utils/database/dev.sql
+++ utils/database/dev.sql
@@ -1,6 +1,12 @@
 -- create user `geo` with password `geo`
 CREATE USER geo WITH ENCRYPTED PASSWORD 'geo';
 
+-- Make geo a superuser
+ALTER USER geo SUPERUSER;
+
+-- Give geo ability to create databases
+ALTER USER geo CREATEDB;
+
 -- create database
 CREATE DATABASE geodjango with OWNER geo;
 
