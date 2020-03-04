602722f5386832ec7679996b8ad7f809dfe08d96
David Fan <fand.kanade@gmail.com>
2020-03-04 01:01:49 -0500

    Updated example websocket test

--- sensors/tests/test_websockets.py
+++ sensors/tests/test_websockets.py
@@ -1,6 +1,14 @@
 import pytest
 from channels.testing import HttpCommunicator
+from channels.testing import WebsocketCommunicator
 from sensors.consumers import SensorConsumer
+
+from channels.generic.websocket import (
+    AsyncJsonWebsocketConsumer,
+    AsyncWebsocketConsumer,
+    JsonWebsocketConsumer,
+    WebsocketConsumer,
+)
 
 @pytest.mark.asyncio
 async def test_my_consumer():
@@ -10,4 +18,38 @@ async def test_my_consumer():
     response = await communicator.get_response()
     assert response["body"] == b"test response"
     assert response["status"] == 200
+    '''
+
+@pytest.mark.asyncio
+async def test_async_websocket_consumer():
+
+    results = {}
+
+    class TestConsumer(AsyncWebsocketConsumer):
+        async def connect(self):
+            results["connected"] = True
+            await self.accept()
+
+        async def receive(self, text_data=None, bytes_data=None):
+            results["received"] = (text_data, bytes_data)
+            await self.send(text_data=text_data, bytes_data=bytes_data)
+
+        async def disconnect(self, code):
+            results["disconnected"] = code
+
+    communicator = WebsocketCommunicator(TestConsumer, "/testws/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+
+    await communicator.disconnect()
+    '''
+    communicator = WebsocketCommunicator(SimpleWebsocketApp, "/testws/")
+    connected, subprotocol = await communicator.connect()
+    assert connected
+    # Test sending text
+    await communicator.send_to(text_data="hello")
+    response = await communicator.receive_from()
+    assert response == "hello"
+    # Close
+    await communicator.disconnect()
     '''
