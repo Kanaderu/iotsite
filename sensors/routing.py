ded064fd71de0e431500edea454585644e6b5eaf
David Fan <fand.kanade@gmail.com>
2020-11-18 05:11:21 +0000

    Fixed version change updates to pytest

--- sensors/routing.py
+++ sensors/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'live/(?P<sensors>\w+)/$', consumers.SensorConsumer),
+    re_path(r'live/(?P<sensors>\w+)/$', consumers.SensorConsumer.as_asgi()),
 ]
